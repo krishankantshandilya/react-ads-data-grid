20f931c894a6034d0c17c0c6050dc59920ea0585
Krishan.Kant <kkshandilya29@gmail.com>
2023-05-29 17:31:06 +0400

    fix style issue for filter card

--- src/components/Datagrid/Datagrid.jsx
+++ src/components/Datagrid/Datagrid.jsx
@@ -1,16 +1,32 @@
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
+import { StylesProvider } from "@mui/styles";
+import { connectorTheme } from "akeneo-design-system";
+import { ThemeProvider } from "styled-components";
+import PropTypes from "prop-types";
 import { persistor, store } from "../../store/index";
 import DatagridTable from "./Table";
 
-const Datagrid = (props) => {
+const Datagrid = ({ theme, ...rest }) => {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <DatagridTable {...props} />
-      </PersistGate>
-    </Provider>
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <DatagridTable {...rest} />
+          </PersistGate>
+        </Provider>
+      </ThemeProvider>
+    </StylesProvider>
   );
+};
+
+Datagrid.defaultProp = {
+  theme: connectorTheme,
+};
+
+Datagrid.propTypes = {
+  theme: PropTypes.object,
 };
 
 export default Datagrid;
